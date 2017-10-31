c7d5660d2715c934e91ff3218245f20adc877250
Luis H. Moreno <luishmcmoreno@gmail.com>
2017-10-31 18:04:47 -0200

    Environments variables

---
+++ config/webpack.config.js
@@ -0,0 +1,33 @@
+var chalk = require('chalk');
+var fs = require('fs');
+var path = require('path');
+var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+
+var env = process.env.PLURI_ENV;
+
+useDefaultConfig.prod.resolve.alias = {
+  '@app/env': path.resolve(environmentPath())
+};
+
+useDefaultConfig.dev.resolve.alias = {
+  '@app/env': path.resolve(environmentPath())
+};
+
+function environmentPath() {
+  var filePath;
+  if (!env) {
+    filePath = './src/environments/environment.dev.ts';
+  } else {
+    filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
+  }
+  console.log(filePath, env);
+  if (!fs.existsSync(filePath)) {
+    console.log(chalk.red('\n' + filePath + ' does not exist!'));
+  } else {
+    return filePath;
+  }
+}
+
+module.exports = function () {
+  return useDefaultConfig;
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { environment } from '@app/env';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public text: string;
 
+  constructor(public navCtrl: NavController) {
+    this.text = environment.text;
   }
 
 }
