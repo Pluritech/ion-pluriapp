cc17e7c54f802fcec9e3cca7e89b214515f16559
Luis H. Moreno <luishmcmoreno@gmail.com>
2017-11-07 18:11:58 -0200

    adding lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 import { environment } from '@app/env';
 
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
